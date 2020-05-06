95337558e9ae1c33ab644d2029b4f2bab9c8a37b
Dominika Wysocka <dwysocka13@gmail.com>
2020-05-06 15:54:53 +0200

    form seria

---
+++ HomeFitness/HomeFitness/Seria.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace HomeFitness
+{
+    partial class Seria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.bazaDataSet = new HomeFitness.bazaDataSet();
+            this.bazaDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.seriacwiczenBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.seria_cwiczenTableAdapter = new HomeFitness.bazaDataSetTableAdapters.Seria_cwiczenTableAdapter();
+            this.seriacwiczenBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.bazaDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bazaDataSetBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seriacwiczenBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seriacwiczenBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(331, 167);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 78);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Dodaj nową serię";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label2.Location = new System.Drawing.Point(12, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(591, 82);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "HOME-FITNESS";
+            // 
+            // bazaDataSet
+            // 
+            this.bazaDataSet.DataSetName = "bazaDataSet";
+            this.bazaDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // bazaDataSetBindingSource
+            // 
+            this.bazaDataSetBindingSource.DataSource = this.bazaDataSet;
+            this.bazaDataSetBindingSource.Position = 0;
+            // 
+            // seriacwiczenBindingSource
+            // 
+            this.seriacwiczenBindingSource.DataMember = "Seria_cwiczen";
+            this.seriacwiczenBindingSource.DataSource = this.bazaDataSetBindingSource;
+            // 
+            // seria_cwiczenTableAdapter
+            // 
+            this.seria_cwiczenTableAdapter.ClearBeforeFill = true;
+            // 
+            // seriacwiczenBindingSource1
+            // 
+            this.seriacwiczenBindingSource1.DataMember = "Seria_cwiczen";
+            this.seriacwiczenBindingSource1.DataSource = this.bazaDataSetBindingSource;
+            // 
+            // splitter1
+            // 
+            this.splitter1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.splitter1.Location = new System.Drawing.Point(763, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(733, 910);
+            this.splitter1.TabIndex = 16;
+            this.splitter1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(819, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(143, 26);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Nazwa serii:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(968, 73);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(383, 26);
+            this.textBox1.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(819, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(121, 26);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "Kategoria:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Nogi",
+            "Ręce",
+            "Plecy",
+            "Brzuch",
+            "Cardio"});
+            this.comboBox1.Location = new System.Drawing.Point(968, 128);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(383, 28);
+            this.comboBox1.TabIndex = 20;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(968, 184);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(383, 28);
+            this.comboBox2.TabIndex = 21;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(819, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 26);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "Ćwiczenie:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(819, 234);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 26);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Ilość:";
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.trackBar1.Location = new System.Drawing.Point(968, 234);
+            this.trackBar1.Maximum = 25;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.trackBar1.Size = new System.Drawing.Size(383, 69);
+            this.trackBar1.TabIndex = 24;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Location = new System.Drawing.Point(968, 384);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(383, 424);
+            this.listBox1.TabIndex = 25;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(819, 329);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(237, 26);
+            this.label6.TabIndex = 26;
+            this.label6.Text = "Wybrane ćwicznenia:";
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 361);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(717, 519);
+            this.treeView1.TabIndex = 27;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(12, 329);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(264, 26);
+            this.label7.TabIndex = 28;
+            this.label7.Text = "Dostępne serie ćwiczeń";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button2.Location = new System.Drawing.Point(62, 167);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(189, 78);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "Podgląd serii ćwiczeń";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button3.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.button3.Location = new System.Drawing.Point(1336, 825);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(137, 55);
+            this.button3.TabIndex = 30;
+            this.button3.Text = "Zapisz";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Seria
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1496, 910);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Name = "Seria";
+            this.Text = "Seria";
+            this.Load += new System.EventHandler(this.Seria_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bazaDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bazaDataSetBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seriacwiczenBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.seriacwiczenBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.BindingSource bazaDataSetBindingSource;
+        private bazaDataSet bazaDataSet;
+        private System.Windows.Forms.BindingSource seriacwiczenBindingSource;
+        private bazaDataSetTableAdapters.Seria_cwiczenTableAdapter seria_cwiczenTableAdapter;
+        private System.Windows.Forms.BindingSource seriacwiczenBindingSource1;
+        private System.Windows.Forms.Splitter splitter1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
