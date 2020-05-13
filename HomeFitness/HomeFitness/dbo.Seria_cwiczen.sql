50dd47de5f1c43ce4177f89aa486dcd091b8ac9e
Dominika Wysocka <dwysocka13@gmail.com>
2020-05-13 09:54:45 +0200

    seria cwiczen autiinkrementacja

---
+++ HomeFitness/HomeFitness/dbo.Seria_cwiczen.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Seria_cwiczen] (
+    [Nr_Serii]        INT       NOT NULL IDENTITY,
+    [Nazwa_serii]     CHAR (32) NULL,
+    [Cwiczona_partia] CHAR (32) NULL,
+    CONSTRAINT [Seria_cwiczen_PK] PRIMARY KEY CLUSTERED ([Nr_Serii] ASC)
+);
+
--- HomeFitness/dbo.Cwiczenia.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Cwiczenia] (
-    [Nr_cwiczenia]     INT     primary key  ,
-    [Nazwa]            CHAR (32) NULL,
-    [Spalone_kalorie]  INT       NULL,
-    [Cwiczone_miesnie] CHAR (32) NULL,
-    [Opis]             TEXT      NULL,
-    [Zalecana_ilosc]   INT       NULL 
-);
-
