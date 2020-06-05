b1d4449aac42ec3114faa25ae469b475d2cc5b98
Kamil Barański <kamilbar99@gmail.com>
2020-06-05 13:27:50 +0200

    Zmiana nazwy

--- HomeFitness/HomeFitness/Form1.Designer.cs
+++ HomeFitness/HomeFitness/Form1.Designer.cs
@@ -180,7 +180,7 @@
             this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "HomeFitness";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.bazaDataSet)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.cwiczeniaBindingSource)).EndInit();
